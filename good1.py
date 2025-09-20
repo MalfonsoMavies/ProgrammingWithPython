57a061c68848009c10a6b1bcd8270500af83401f
Matti Al hanota <alma23lj@student.ju.se>
2025-09-20 13:16:20 +0200

    order1 and good1

---
+++ good1.py
@@ -0,0 +1,9 @@
+x = int(input())
+if x >= 4 and x <= 11:
+    print("Good morning")
+elif x >= 12 and x <= 17:
+    print("Good afternoon")
+elif x >= 18 and x <= 23:
+    print("Good evening")
+else:
+    print("Hi")
